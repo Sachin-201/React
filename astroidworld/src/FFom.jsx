63bf11cac87ccd28ffe2d557e2c81852ad03760e
Sachin-201 <156083024+Sachin-201@users.noreply.github.com>
2024-01-16 16:02:04 +0530

    Update and rename FFom.js to FFom.jsx

---
+++ astroidworld/src/FFom.jsx
@@ -0,0 +1,96 @@
+import { useNavigate } from "react-router-dom";
+import "./FFom.css";
+import { useState } from "react";
+const APIKEY = "rsl9CbhucQ2J9DsQb7nBNd4qWrqC11T9SdUOwSSk";
+const baseurl = "https://api.nasa.gov/neo/rest/v1/neo/";
+
+function FFom() {
+  const navigate = useNavigate();
+  let [Astoid, setAstoid] = useState("");
+  let [Apicall, SetApicall] = useState(false);
+
+  const Detailcall = async (data) => {
+    navigate("/Details", { state: { data: data } });
+  };
+
+  const Astrosubmit = async () => {
+    try {
+      console.log(Astoid);
+      console.log("data ype of Astoid ", typeof Astoid);
+      if (Astoid === "") {
+        alert("Please Enter A Valid Astroid ID don't enter null ");
+        return;
+      }
+      console.log("Submit is called", Astoid);
+      SetApicall(true);
+      const response = await fetch(baseurl + Astoid + "?api_key=" + APIKEY);
+      let data = await response.json();
+      data = JSON.stringify(data);
+      Detailcall(data);
+    } catch (error) {
+      alert("Please Enter A Valid Astroid ID ");
+    } finally {
+      SetApicall(false);
+    }
+  };
+
+  const RAstrosubmit = async () => {
+    try {
+      SetApicall(false);
+      const response = await fetch(baseurl + "browse?api_key=" + APIKEY);
+      let data = await response.json();
+      let APIDATA = [{}];
+      APIDATA = data.near_earth_objects.map((item) => {
+        return item;
+      });
+      let randomdata = APIDATA[Math.floor(Math.random() * APIDATA.length)];
+      randomdata = JSON.stringify(randomdata);
+      Detailcall(randomdata);
+    } catch (error) {
+      alert("Error occured Please refresh ", error);
+    } finally {
+      SetApicall(false);
+    }
+  };
+
+  return (
+    <div>
+      <div className="login">
+        <div className="form">
+          <h1>Welcome To the Astroid World</h1>
+          <div>
+            <label>
+              <br></br>
+              <br></br>
+              <input
+                type="number"
+                onChange={(e) => setAstoid(e.target.value)}
+                placeholder="Enter a Astroid ID"
+              />
+              <br />
+              <button
+                type="submit"
+                disabled={Apicall && Astoid.length === 7} //yha toda error lag rha hai
+                className="btn"
+                onClick={Astrosubmit}
+              >
+                Submit
+              </button>
+              <button
+                type="submit"
+                disabled={Apicall}
+                className="btn"
+                onClick={RAstrosubmit}
+              >
+                Random
+              </button>
+              <br />
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FFom;
