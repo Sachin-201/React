26972d6471f4ed99b7747adcceefbb3c12af2086
Sachin-201 <156083024+Sachin-201@users.noreply.github.com>
2024-01-18 01:25:54 +0530

    Update index.js

--- astroidworld/src/index.js
+++ astroidworld/src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import Details from './Details'
-// import FFom from './FFom'
-// import { createBrowserRouter } from 'react-router-dom';
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,11 +10,5 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-
-    {/* <div>
-         <App />
-        <FFom/> 
-         <Details/>
-        </div> */}
   </React.StrictMode>
 );
